5955e8d26645aeecddc780e13fdb569b592cb450
Michal <46096121+jedrzej9@users.noreply.github.com>
2020-12-02 00:08:03 +0100

    commit

--- triple-X/triple-X.cpp
+++ triple-X/triple-X.cpp
@@ -1,11 +1,59 @@
-// triple-X.cpp : This file contains the 'main' function. Program execution begins and ends there.
+﻿// triple-X.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
+#include <ctime>
+
+void playGameAtDifficulty(int difficulty) {
+    std::cout << "You run to door number " << difficulty << " with a three digit locker\n";
+    std::cout << "On the doors are also written the following instructions:\n";
+
+    srand(time(NULL));
+
+    const int a = rand() % difficulty + difficulty;
+    const int b = rand() % difficulty + difficulty;
+    const int c = rand() % difficulty + difficulty;
+
+    const int sum = a + b + c;
+    const int product = a * b * c;
+
+    std::cout << std::endl;
+    std::cout << "+ There are three numbers in the code\n";
+    std::cout << "+ The codes multiply to give " << product << std::endl;
+    std::cout << "+ The codes add-up to " << sum << std::endl;
+
+    std::cout << std::endl;
+    std::cout << "Enter the three numbers followed by x\n";
+    std::cout << std::endl;
+    
+    int answer;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //TO-DO zrobić z tego tablicę i wyświetlać po literze z przerwami
+    std::cout << "You're in a long corridor. At the first end there is only darkness.\n";
+    std::cout << "At the second end you see door\n";
+    std::cout << "Suddenly you can hear sounds coming to you from the first end\n";
+    std::cout << "Then you see it...\n";
+    std::cout << "It have disfigured eyes, with flies buzz in and out\n";
+    std::cout << "Monstrously large, three-fingered claws";
+    std::cout << "Unnaturally long legs covered with fur\n";
+    std::cout << "And this creature creature has just begun to approach you!!!\n";
+
+    int difficulty = 2;
+    int maxDifficulty = 10;
+
+    while (difficulty <= maxDifficulty)
+    {
+        playGameAtDifficulty(difficulty);
+        std::cin.clear();
+        std::cin.ignore();
+        difficulty++;
+    }
+
+    std::cout << "YOU ESCAPED !!! Congratulations !!!\n";
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
